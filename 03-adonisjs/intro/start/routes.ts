7bc0bf556cb925047f6df78fd2eccabf788feb32
prof_riemke <2960904+profriemke@users.noreply.github.com>
2025-04-08 17:30:15 +0200

    LV08.04. (2)

--- 03-adonisjs/intro/start/routes.ts
+++ 03-adonisjs/intro/start/routes.ts
@@ -9,10 +9,33 @@
 
 import router from '@adonisjs/core/services/router'
 
+router.get('/nutzer', async ({ view })=>{
+    return view.render('pages/nutzer')
+})
+
+router.post('/nutzer/anzeige', async ({view, request})=>{
+    let daten = { vorname: request.input('vorname'),
+                  nachname: request.input('nachname')}              
+    return view.render('pages/anzeige', daten)
+})
+
 router.get('/', async ({ view })=>{ 
-    const state = {gruss: 'nabend'}
+    const state = {gruss: 'Guten Mittag', namen: ['Lisa', 'Paul', 'Malte']}
+    console.log(await view.render('pages/meinStart', state))
     return view.render('pages/meinStart', state)
 })
+
+router.get('/personen', async ({ view })=>{
+    const data = {
+        isLoggedIn: false,
+        personen:[
+        {nachname:'Müller', vorname:'Malte'},
+        {nachname:'Lauer', vorname:'Lisa'},
+        {nachname:'Summ', vorname:'Sanja'}
+    ] }
+    return view.render('pages/personen', data)
+})
+
 
 router.get('/omm', async ()=>{
     return 'Beschte wo gibt'
