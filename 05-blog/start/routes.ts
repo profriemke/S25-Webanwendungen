adce9a452b5ddce527131c185da6b1f811163276
prof_riemke <2960904+profriemke@users.noreply.github.com>
2025-04-22 15:49:20 +0200

    LV 22.04. (2)

--- 05-blog/start/routes.ts
+++ 05-blog/start/routes.ts
@@ -8,5 +8,34 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import db from '@adonisjs/lucid/services/db'
 
-router.on('/').render('pages/home')
+
+router.get('/', async ({ view })=>{
+    const posts = await db.from('posts').select('*').orderBy('id', 'desc')    
+    return view.render('pages/home', { posts })
+})
+
+router.get('/admin/post/create', async ({ view })=>{
+    return view.render('pages/admin_post_create')
+
+})
+
+router.post('/admin/post/create', async ({ request, response })=>{
+    const result = await  db.table('posts')
+                            .insert({
+                                title: request.input('title'),
+                                teaser: request.input('teaser'),
+                                text: request.input('text'),
+                                date: new Date().toString()
+                            })
+    return response.redirect('/')
+})
+
+router.get('/post/:id', async ({ view, params})=>{
+    const post = await db.from('posts')
+                         .select('*')
+                         .where('id', params.id)
+                         .first()
+     return post                    
+})
